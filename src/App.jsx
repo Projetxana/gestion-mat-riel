2292605bf871342c8e661a606732810fd504acc7
Jerome Recipon <jerome.recipon@gmail.com>
2026-06-19 17:33:50 -0400

    Bump version 0.9.12

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import GlobalTimer from './components/GlobalTimer';
 
 const VersionOverlay = () => (
   <div className="fixed bottom-0 right-0 bg-black/80 text-white text-[10px] p-1 z-[9999] pointer-events-none">
-    v0.9.11
+    v0.9.12
   </div>
 );
 import LoginPage from './pages/LoginPage';
